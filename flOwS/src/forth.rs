5c39e133b498a07fce57a1b35f50c0c4d80191cb
Dmitry Ponyatov <dponyatov@gmail.com>
2020-11-26 14:09:35 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- flOwS/src/forth.rs
+++ flOwS/src/forth.rs
@@ -1,1 +1,21 @@
-pub fn forth() {}
+pub fn forth() {
+    println!("FORTH");
+    interpret();
+}
+
+static mut stack: i32 = 0x12345678;
+
+fn interpret() {
+    loop {
+        bl();
+        q();
+    }
+}
+
+fn bl() {
+    stack = 0x20; // space
+}
+
+fn q() {
+    println!("stack: {:?}", stack);
+}
