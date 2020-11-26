a4308f5fdac35d02faf23a5e5abf148d9c68ca92
Dmitry Ponyatov <dponyatov@gmail.com>
2020-11-26 12:27:51 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- flOwS/README.md
+++ flOwS/README.md
@@ -19,6 +19,10 @@ github: https://github.com/ponyatov/flOwS
 
 ### Rust
     
+* [aleksey.kladov](https://www.youtube.com/playlist?list=PLlb7e2G7aSpTfhiECYNI2EZ1uAluUqE_e)
+    * [1. Введение (Программирование на Rust)](https://www.youtube.com/watch?v=Oy_VYovfWyo)
+    * [2. Время жизни, ADT. Программирование на Rust (весна 2019)](https://www.youtube.com/watch?v=WV-m7xRlXMs)
+
 * [Rust CS196 FA20](https://www.youtube.com/playlist?list=PLddc343N7YqhSPMjlCJa1gRDt4CzjiMYZ)
     * [Welcome to 196! - CS196 FA20](https://www.youtube.com/watch?v=J__JvfNuknU&list=PLddc343N7YqhSPMjlCJa1gRDt4CzjiMYZ&index=1&t=795s)
     * [Rust 1 - Lecture 17 - CS196 FA20](https://www.youtube.com/watch?v=ac7AOtkQMx4)
--- metaL.py
+++ metaL.py
@@ -418,6 +418,15 @@ class rsModule(dirModule):
     def init_readme(self):
         super().init_readme()
         self.d.readme.rust = S('### Rust', pfx='')
+        self.d.readme.rust // '''
+* [aleksey.kladov](https://www.youtube.com/playlist?list=PLlb7e2G7aSpTfhiECYNI2EZ1uAluUqE_e)
+    * [1. Введение (Программирование на Rust)](https://www.youtube.com/watch?v=Oy_VYovfWyo)
+    * [2. Время жизни, ADT. Программирование на Rust (весна 2019)](https://www.youtube.com/watch?v=WV-m7xRlXMs)
+
+* [Rust CS196 FA20](https://www.youtube.com/playlist?list=PLddc343N7YqhSPMjlCJa1gRDt4CzjiMYZ)
+    * [Welcome to 196! - CS196 FA20](https://www.youtube.com/watch?v=J__JvfNuknU&list=PLddc343N7YqhSPMjlCJa1gRDt4CzjiMYZ&index=1&t=795s)
+    * [Rust 1 - Lecture 17 - CS196 FA20](https://www.youtube.com/watch?v=ac7AOtkQMx4)
+'''
         self.d.readme // self.d.readme.rust
 
     def init_mk(self):
