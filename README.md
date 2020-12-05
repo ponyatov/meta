f5714a815ff89a8f7420a29ed56b55c5d516212f
Dmitry Ponyatov <dponyatov@gmail.com>
2020-12-05 23:54:03 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+#  `metaL`
+## Homoiconic [meta]programming [L]anguage / [L]ayer
+
+(c) Dmitry Ponyatov <<dponyatov@gmail.com>> 2020 MIT
+
+* Discord: https://discord.gg/5CYZdt6
+* [book drafts](https://www.notion.so/metalang/Wiki-18ae2c8192bd4b5c8548bf7f56f390d6) en/ru
+  * [`metaL` manifest](https://www.notion.so/metalang/metaL-manifest-f7c2e3c9f4494986a620f3a71cf39cff)
+  * [Distilled `metaL`](https://www.notion.so/metalang/Distilled-metaL-SICP-chapter-4-237378d385024f899e5a24597da7a19d)
+  * [глава 4 Металингвистическая абстракция](https://www.notion.so/metalang/4-eb7dfcf3dbb04e6eb8015337af850aab)
+    (частичный перевод с адаптацией)
+
+github: https://github.com/ponyatov/metaL
--- circ.py
+++ circ.py
@@ -75,16 +75,16 @@ class thisModule(pyModule):
 
 
 mod = thisModule()
-# mod.MODULE = 'metaL'
-# mod.TITLE = 'Homoiconic [meta]programming [L]anguage / [L]ayer'
-# mod.ABOUT = """
-# * `github`: https://github.com/ponyatov/metaL
-# * Discord: https://discord.gg/5CYZdt6
-# * [book drafts](https://www.notion.so/metalang/Wiki-18ae2c8192bd4b5c8548bf7f56f390d6) en/ru
-#   * [`metaL` manifest](https://www.notion.so/metalang/metaL-manifest-f7c2e3c9f4494986a620f3a71cf39cff)
-#   * [Distilled `metaL`](https://www.notion.so/metalang/Distilled-metaL-SICP-chapter-4-237378d385024f899e5a24597da7a19d)
-#   * [глава 4 Металингвистическая абстракция](https://www.notion.so/metalang/4-eb7dfcf3dbb04e6eb8015337af850aab)
-#     (частичный перевод с адаптацией)
+mod.MODULE = 'metaL'
+mod.TITLE = 'Homoiconic [meta]programming [L]anguage / [L]ayer'
+mod.ABOUT = """
+* Discord: https://discord.gg/5CYZdt6
+* [book drafts](https://www.notion.so/metalang/Wiki-18ae2c8192bd4b5c8548bf7f56f390d6) en/ru
+  * [`metaL` manifest](https://www.notion.so/metalang/metaL-manifest-f7c2e3c9f4494986a620f3a71cf39cff)
+  * [Distilled `metaL`](https://www.notion.so/metalang/Distilled-metaL-SICP-chapter-4-237378d385024f899e5a24597da7a19d)
+  * [глава 4 Металингвистическая абстракция](https://www.notion.so/metalang/4-eb7dfcf3dbb04e6eb8015337af850aab)
+    (частичный перевод с адаптацией)
+"""
 
 # ## Language Ideas Promo
 
